1fca7a7b339ab6ba8ca0943795669df533d2720e
Conor Mullan <conor.mullan@unosquare.com>
2022-03-01 13:44:22 +0000

    Create V1.1__adding_referential_data.sql

---
+++ migrations/base/V1.1__adding_referential_data.sql
@@ -0,0 +1,10 @@
+INSERT INTO consumer_type (id, description) VALUES 
+(1, 'admin'),
+(2, 'user');
+
+INSERT INTO item_consumer_type (id, description) VALUES 
+(1,'owner'),
+(2, 'restricted'),
+(3, 'consumable');
+
+
